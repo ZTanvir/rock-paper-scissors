2e0a3cd0fad83660b905c01916bb1944ac4c40a1
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-06-25 13:30:22 +0600

    OneRoundPlay function will play the game for 1 round and return won,lose,tie
    Added oneRoundPlay function the app.js file It's added for to check the winner,
    loser and draw for a single round of rock,paper,scissors game
    
    By tacking userInput and computerInput as parameter of oneRoundPlay function.
    First we turn the userInput to lowerCase() so it wont conflict with different
    userInput.Then we have check user input vs computerInput against when userInput
    rock,paper,and scissor

--- script/app.js
+++ script/app.js
@@ -1,17 +1,71 @@
-// Rock paper scissors game logic
+// Rock paper scissors Game
+//Select HTML Nodes
+let userWon = document.querySelector(".player-won");
+let computerwon = document.querySelector(".computer-won");
+let totalTie = document.querySelector(".total-tie");
 
 // computerPlay return the option select by computer
 
-let computerPlay = () =>{
-    // Computer all choose option
-    let computerAllOptions = "rock,paper,scissors"
-        // Divide them to indivisual item
-    let divideComputerOptions = computerAllOptions.split(",")
-    // Computer selected option
-        // Genarate random number between 0 - 2
-    let randomInt = Math.floor(Math.random() * 3)
-    let computerChoice = divideComputerOptions[randomInt]
-    // return the selected option by computer
-    return computerChoice
-}
-console.log(computerPlay())
+let computerPlay = () => {
+  // Computer all choose option
+  let computerAllOptions = "rock,paper,scissors";
+  // Divide them to indivisual item
+  let divideComputerOptions = computerAllOptions.split(",");
+  // Computer selected option
+  // Genarate random number between 0 - 2
+  let randomInt = Math.floor(Math.random() * 3);
+  let computerChoice = divideComputerOptions[randomInt];
+  // return the selected option by computer
+  return computerChoice;
+};
+// Play one round of rock,paper,scissors
+
+let oneRoundPlay = (userInput, computerInput) => {
+    // Take userInput and convert it to lowercase
+    userInput = userInput.toLowerCase();
+    console.log("User choose:", userInput);
+    console.log("Computer choose:", computerInput);
+    // Check userInput vs Computer Input the game logic is
+    // rock > scissors,paper > rock , scissors > paper
+    // print is the user won or lose based on the game logic
+  
+    // When user choose rock
+    if (userInput === "rock") {
+      if (computerInput === "scissors") {
+        console.log("You won! Rock beats Scissors");
+        return "won";
+      } else if (computerInput === "rock") {
+        console.log("Tie! You both pick Rock");
+        return "tie";
+      } else if (computerInput === "paper") {
+        console.log("You lose ! Paper beats Rock");
+        return "lose";
+      }
+    }
+    // When user choose paper
+    else if (userInput === "paper") {
+      if (computerInput === "rock") {
+        console.log("You won! Paper beats Rock");
+        return "won";
+      } else if (computerInput === "paper") {
+        console.log("Tie! You both pick Paper");
+        return "tie";
+      } else if (computerInput === "scissors") {
+        console.log("You lose ! Scissors beats Paper");
+        return "lose";
+      }
+    }
+    //   When user choose scissors
+    else if (userInput === "scissors") {
+      if (computerInput === "paper") {
+        console.log("You won! Scissors beats Paper");
+        return "won";
+      } else if (computerInput === "scissors") {
+        console.log("Tie! You both pick Scissors");
+        return "tie";
+      } else if (computerInput === "rock") {
+        console.log("You lose ! Rock beats Scissors");
+        return "lose";
+      }
+    }
+  };
