557e1740b293491f8b2aaceca5156b1fabdfac4f
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-06-26 10:42:55 +0600

    Add the game function which will run the function oneRoundPlay for 5 rounds
    Created a game function which will calculate total win,lose,tie of both
    user,computer.
    Last time the game can be play for only one round,but using this function
    it can be player 5 rounds.
    Using a loop that will run 5 time each time user input their option and if
    the game tie,won,lose this will shown in the index.html page.

--- script/app.js
+++ script/app.js
@@ -7,16 +7,16 @@ let totalTie = document.querySelector(".total-tie");
 // computerPlay return the option select by computer
 
 let computerPlay = () => {
-  // Computer all choose option
-  let computerAllOptions = "rock,paper,scissors";
-  // Divide them to indivisual item
-  let divideComputerOptions = computerAllOptions.split(",");
-  // Computer selected option
-  // Genarate random number between 0 - 2
-  let randomInt = Math.floor(Math.random() * 3);
-  let computerChoice = divideComputerOptions[randomInt];
-  // return the selected option by computer
-  return computerChoice;
+    // Computer all choose option
+    let computerAllOptions = "rock,paper,scissors";
+    // Divide them to indivisual item
+    let divideComputerOptions = computerAllOptions.split(",");
+    // Computer selected option
+    // Genarate random number between 0 - 2
+    let randomInt = Math.floor(Math.random() * 3);
+    let computerChoice = divideComputerOptions[randomInt];
+    // return the selected option by computer
+    return computerChoice;
 };
 // Play one round of rock,paper,scissors
 
@@ -28,44 +28,80 @@ let oneRoundPlay = (userInput, computerInput) => {
     // Check userInput vs Computer Input the game logic is
     // rock > scissors,paper > rock , scissors > paper
     // print is the user won or lose based on the game logic
-  
+
     // When user choose rock
     if (userInput === "rock") {
-      if (computerInput === "scissors") {
-        console.log("You won! Rock beats Scissors");
-        return "won";
-      } else if (computerInput === "rock") {
-        console.log("Tie! You both pick Rock");
-        return "tie";
-      } else if (computerInput === "paper") {
-        console.log("You lose ! Paper beats Rock");
-        return "lose";
-      }
+        if (computerInput === "scissors") {
+            console.log("You won! Rock beats Scissors");
+            return "won";
+        } else if (computerInput === "rock") {
+            console.log("Tie! You both pick Rock");
+            return "tie";
+        } else if (computerInput === "paper") {
+            console.log("You lose ! Paper beats Rock");
+            return "lose";
+        }
     }
     // When user choose paper
     else if (userInput === "paper") {
-      if (computerInput === "rock") {
-        console.log("You won! Paper beats Rock");
-        return "won";
-      } else if (computerInput === "paper") {
-        console.log("Tie! You both pick Paper");
-        return "tie";
-      } else if (computerInput === "scissors") {
-        console.log("You lose ! Scissors beats Paper");
-        return "lose";
-      }
+        if (computerInput === "rock") {
+            console.log("You won! Paper beats Rock");
+            return "won";
+        } else if (computerInput === "paper") {
+            console.log("Tie! You both pick Paper");
+            return "tie";
+        } else if (computerInput === "scissors") {
+            console.log("You lose ! Scissors beats Paper");
+            return "lose";
+        }
     }
     //   When user choose scissors
     else if (userInput === "scissors") {
-      if (computerInput === "paper") {
-        console.log("You won! Scissors beats Paper");
-        return "won";
-      } else if (computerInput === "scissors") {
-        console.log("Tie! You both pick Scissors");
-        return "tie";
-      } else if (computerInput === "rock") {
-        console.log("You lose ! Rock beats Scissors");
-        return "lose";
-      }
+        if (computerInput === "paper") {
+            console.log("You won! Scissors beats Paper");
+            return "won";
+        } else if (computerInput === "scissors") {
+            console.log("Tie! You both pick Scissors");
+            return "tie";
+        } else if (computerInput === "rock") {
+            console.log("You lose ! Rock beats Scissors");
+            return "lose";
+        }
     }
-  };
+};
+
+// Test oneRoundPlay() function
+// console.log(oneRoundPlay("Scissors", computerPlay()));
+
+// Play the game
+let game = () => {
+    let playerWon = 0;
+    let computerWon = 0;
+    let tie = 0;
+
+    // Total round play
+    for (let i = 0; i < 5; i++) {
+        // Get user and computer choice
+        let userChoice = prompt("Choose rock,paper,scissors:");
+        let computerChoice = computerPlay();
+        // Result on one round
+        let oneRoundResult = oneRoundPlay(userChoice, computerChoice);
+        // Prepare point based on oneRoundResult
+        if (oneRoundResult == "won") {
+            playerWon++;
+        } else if (oneRoundResult == "tie") {
+            tie++;
+        } else if (oneRoundResult == "lose") {
+            computerWon++;
+        }
+    }
+    //   Result
+    console.log("playerWon:", playerWon);
+    userWon.textContent = playerWon;
+    console.log("computerWon:", computerWon);
+    computerwon.textContent = computerWon;
+    console.log("tie:", tie);
+    totalTie.textContent = tie;
+};
+// Start the game :)
+// game();
